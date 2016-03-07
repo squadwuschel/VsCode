30cf0118339e13968d5d52c6753fe8e9ebd67dd8
SquadWuschel <joecorporation@gmx.de>
2016-03-07 22:22:32 +0100

    Compile TypeScript

--- app.ts
+++ app.ts
@@ -1,27 +1,28 @@
-module App {
-    
-    /**
-     * Person
-     */
-    export class Person {
-        constructor(public name: string) {
 
-        }
+    module App {
+        
+        /**
+         * Person
+         */
+        export class Person {
+            constructor(public name: string) {
+
+            }
 
 
-        public log(showLog: boolean): void {
-            if (showLog) {
-                console.log("Der Name des Nutzers ist: " + this.name)
+            public log(showLog: boolean): void {
+                if (showLog) {
+                    console.log("Der Name des Nutzers ist: " + this.name)
+                }
             }
         }
+ 
+
+
     }
 
+    //VS Code for TypeScript
+    //https://code.visualstudio.com/Docs/languages/typescript
 
-
-}
-
-//VS Code for TypeScript
-//https://code.visualstudio.com/Docs/languages/typescript
-
-var person = new App.Person("Hallo Welt");
-person.log(true);
+    var person = new App.Person("Hallo Welt");
+    person.log(true);
